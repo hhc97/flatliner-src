a80bf4eef1d9d3cfaac20deff0d313717373d7b8
Haocheng Hu <hhu@zynga.com>
2022-02-08 12:12:34 -0500

    add some comments

--- sprint1/code_examples/for_loops.py
+++ sprint1/code_examples/for_loops.py
@@ -1,5 +1,13 @@
+"""
+A function definition with a function call.
+Test comments with weird characters:
+!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
+"""
+
+
 def test(loops: int) -> int:
-    total = 0
+    total = 0  # total is 0 at first
+    # do the loop
     for i in range(loops):
         total += i
     return total
