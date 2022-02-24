21c0c2c214bcd6b297d5f2718bf14f9f1affe01a
Haocheng Hu <hhu@zynga.com>
2022-02-24 15:09:42 -0500

    add some lambda helpers

--- sprint2/unparser.py
+++ sprint2/unparser.py
@@ -3,6 +3,26 @@ import ast
 from python_parser import PythonParser
 
 
+def construct_lambda(vals: dict[str, str], body: str = '{}') -> str:
+    """
+    Returns a lambda function string with the arguments provided.
+    >>> construct_lambda({'a': '1', 'b': '2'})
+    '(lambda a, b: {})(1, 2)'
+    >>> exec(construct_lambda({'a': '1', 'b': '2'}, 'print(a + b)'))
+    3
+    """
+    return f'(lambda {", ".join(vals.keys())}: {body})({", ".join(vals.values())})'
+
+
+def wrap_globals(body: str):
+    """
+    Wraps a lambda body with the globals dict.
+    >>> wrap_globals('print(a)')
+    '(lambda __g: print(a))(globals())'
+    """
+    return construct_lambda({'__g': 'globals()'}, body)
+
+
 class Unparser:
     def __init__(self):
         self.ast = None
@@ -16,15 +36,26 @@ class Unparser:
             parser.build()
             self.ast = parser.get_ast(f.read())
 
-    def unparse(self):
+    def unparse(self, root=None):
         """
         Unparses the ast.
         """
-        if self.ast is not None:
-            print(ast.dump(self.ast, indent=4))
+        # if self.ast is not None:
+        #     print(ast.dump(self.ast, indent=4))
+        curr = root if root else self.ast
+        print(curr)
+        print(dir(curr))
+        if hasattr(curr, 'body') and isinstance(curr.body, list):
+            for node in curr.body:
+                print('f', node)
+                self.unparse(node)
 
 
 if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
     test = Unparser()
     test.set_ast('test_input.py')
+    print(ast.dump(test.ast, indent=4))
     test.unparse()
