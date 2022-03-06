6a9b6208c2cd2035269f6dd52638e8157bdf5910
Naaz Sibia <sibianjali@gmail.com>
2022-03-06 00:03:29 -0500

    Read if statements

--- sprint2/3ac_to_code.py
+++ sprint2/3ac_to_code.py
@@ -1,39 +1,58 @@
 import ast
 from os import stat
+
+from numpy import var
 from python_parser import PythonParser
 from code_to_3ac import ASTVisitor
 
 code = []
-
+START = 'main'
 class TACConverter:
 
     def __init__(self, tac):
-        self.code = []
+        #self.code = []
         self.tac = tac
         self.lineno = 0
 
-    def convert(self, start = 'main'):
+    def convert(self, statements = None, var_d=None):
+        if statements is None:
+            statements = self.tac[START] #start
+        if var_d is None: var_d = {} # store variables here
+        code = []
+        index = 0
+        while index < len(statements):
+            statement = statements[index]
+            index += 1
+            op = statement[0]
+            var = statement[3]
+            if op == 'IF':
+                # everything till GOTO is else
+                else_block = statements[index:]
+                self.if_handler(statement, var_d, else_block, code)
+                return code # it'll handle the rest of the main block too
+            if op == 'GOTO':
+                return code
+            self.handle_statement(statement, var_d, code)
+        
+        return code
+            
+    def handle_statement(self, statement, var_d, code):
+        op, var = statement[0], statement[3]
         handlers = {'+': self.binary_handler, '-': self.binary_handler, '*': self.binary_handler, '/': self.binary_handler,
                     'OR': self.bool_handler, 'AND': self.bool_handler, '==': self.comp_handler, '>': self.comp_handler,
                     '<': self.comp_handler, '<=': self.comp_handler, '>=': self.comp_handler,
-                    'IF': self.if_handler,
                     '=': self.assignment_handler,
                     'IN': self.comp_handler
                     }
-        var_d = {} # store variables here
-        for statement in self.tac[start]:
-            #op, left, right, var = statement
-            op = statement[0]
-            var = statement[3]
-            node = handlers[op](statement, var_d)
-            if var:
-                var_d[var] = node
-            if var and var.startswith('t_'):
+        node = handlers[op](statement, var_d)
+        if var:
+            var_d[var] = node
+        
+        if var and var.startswith('t_'):
                 # don't count temp vars
-                continue
-            self.code.append(node)
-            self.lineno += 1
-            
+                return
+        code.append(node)
+        self.lineno += 1
 
     def constant_handler(self, constant, var_d):
         type_map = {
@@ -42,8 +61,11 @@ class TACConverter:
             bool: 'bool',
             float: 'float'
         }
-        return var_d.get(constant, 
-                ast.Constant(constant, type_map[type(constant)]))
+        if constant in var_d:
+            if not constant.startswith('t_'):
+                return ast.Name(constant, ast.Load())
+            return var_d[constant]
+        return ast.Constant(constant, type_map[type(constant)])
 
     def binary_handler(self, statement, var_d):
         print('BIN')
@@ -90,9 +112,31 @@ class TACConverter:
         right = self.constant_handler(right, var_d)
         return ast.Compare(left, [op_map[op]], [right])
 
-    def if_handler(self, statement, var_d):
+    def if_handler(self, statement, var_d, else_block, outer_code):
         print('IF')
-        pass
+        cond = var_d[statement[1]]
+        if_block = statement[3]
+        index = 0
+        while index < len(else_block):
+            statement = else_block[index]
+            op, var = statement[0], statement[3]
+            if var and var.startswith('t_'):
+                # need to load temp variables
+                self.handle_statement(statement, var_d, [])
+            else:
+                break
+            index += 1
+        body = self.convert(self.tac[if_block], var_d.copy())
+        else_block = self.convert(else_block[index:], var_d.copy())
+
+        # handles elif to prevent duplication
+        if statement[0] == 'IF':
+            else_block = else_block[:1]
+
+        outer_code.append(ast.If(cond, body, else_block))     
+        goto_block = self.tac[if_block][-1][3]
+        # add goto code to outer code
+        outer_code.extend(self.convert(self.tac[goto_block], var_d))
 
     
 if __name__ == '__main__':
@@ -105,7 +149,8 @@ if __name__ == '__main__':
     print(visitor.tac)
 
     converter = TACConverter(visitor.tac)
-    converter.convert()
-    wrap = ast.Module(converter.code, [])
-    print(ast.unparse(wrap))
+    code = converter.convert()
+    wrap = ast.Module(code, [])
+    #print(ast.dump(wrap, indent=4))
+    print(f'Code:\n{ast.unparse(wrap)}')
     
