c730fc94dd216dd983e96328419232b640bcbcd3
Haocheng Hu <hc.hu@utoronto.ca>
2022-06-22 21:28:50 -0700

    remove s0-s3

--- sprint1/code_examples/for_loops.py
+++
@@ -1,9 +0,0 @@
-def test(loops):
-    total = 0  # total is 0 at first
-    # do the loop
-    for i in range(loops):
-        total = total + i
-    return total
-
-
-print(test(6))
--- sprint3/tac_ast_examples/while_loops.py
+++
@@ -1,9 +0,0 @@
-i = 0
-j = 0
-k = 4
-while i < j:
-    while j > i:
-        i = i + 1
-    while j < k:
-        while j == 2:
-            j = j + 1
